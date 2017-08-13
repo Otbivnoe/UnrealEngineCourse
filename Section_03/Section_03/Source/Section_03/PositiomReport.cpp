46b4c3355e23bdf5935183dd43652565df471951
Nikita Ermolenko <gnod94@gmail.com>
2017-08-13 23:43:12 +0600

    Section3: 53-64

---
+++ Section_03/Section_03/Source/Section_03/PositiomReport.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositiomReport.h"
+#import "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UPositiomReport::UPositiomReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositiomReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+    FString ObjectName = GetOwner()->GetName();
+    FString ObjectPosition = GetOwner()->GetTransform().GetLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s with wthe position %s"), *ObjectName, *ObjectPosition);
+}
+
+
+// Called every frame
+void UPositiomReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
